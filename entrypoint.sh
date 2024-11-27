162b38684a106035f4827ab07aad500234be3fa8
hnaciri-1337 <haithamnaciribensaghir@gmail.com>
2024-11-27 18:41:30 +0100

    specify python version

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,13 +10,14 @@ python manage.py migrate
 python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
-User.objects.create_superuser('admin', 'admin@um6p.ma', 'admin')
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@um6p.ma', 'admin')
 
-User.objects.create_user('user', 'user@um6p.ma', 'user')
-User.objects.create_user('imad.kissami', 'imad.kissami@um6p.ma', 'imad.kissami')
-User.objects.create_user('fahd.kalloubi', 'fahd.kalloubi@um6p.ma', 'fahd.kalloubi')
-User.objects.create_user('hamza.lachkar', 'hamza.lachkar@um6p.ma', 'hamza.lachkar')
-User.objects.create_user('haitham.naciri', 'haitham.naciri@um6p.ma', 'haitham.naciri')
+    User.objects.create_user('user', 'user@um6p.ma', 'user')
+    User.objects.create_user('imad.kissami', 'imad.kissami@um6p.ma', 'imad.kissami')
+    User.objects.create_user('fahd.kalloubi', 'fahd.kalloubi@um6p.ma', 'fahd.kalloubi')
+    User.objects.create_user('hamza.lachkar', 'hamza.lachkar@um6p.ma', 'hamza.lachkar')
+    User.objects.create_user('haitham.naciri', 'haitham.naciri@um6p.ma', 'haitham.naciri')
 User.objects.create_user('abdessamad.laamimi', 'abdessamad.laamimi@um6p.ma', 'abdessamad.laamimi')
 EOF
 
